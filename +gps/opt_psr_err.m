9a75fbf6bffadbeebaf22330e1ec9f2786fa22e0
Robert Cofield <robertgcofield@gmail.com>
2013-10-09 13:02:59 -0500

    removed stuff from rgc_matlab, added what may be the jpl precise ephem file

--- +gps/opt_psr_err.m
+++
@@ -1,4 +0,0 @@
-function out = opt_psr_err()
-% OPT_PSR_ERR is the optimization function wrapping PSR_ERR
-
-end
---
+++ lab3/part1c.m
@@ -0,0 +1,10 @@
+%% Lab 3, Part I, b)
+% Robert Cofield
+% created 2013-10-09
+% 
+
+clear all; close all; clc
+fprintf('\nPart 1 - c)\n')
+load(['..' filesep 'data' filesep 'Novatel_Data.mat'])
+clear time
+
