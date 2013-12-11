6221ef0ec51566aacc31bd698b802941ef13c148
William McGinnis <wdm0006@auburn.edu>
2013-12-11 16:15:04 -0600

    doppler code added

---
+++ proj/calcDoppler.m
@@ -0,0 +1,16 @@
+function dop=calcDoppler(user_v, sat_v, dir_v)
+%user velocity vector, satelltie velocity vector, direction vector (user to
+%satellite).  All in same reference frame.
+
+c=3e8;
+
+dir_v=dir_v./norm(dir_v);
+v_rel=user_v-sat_v;
+
+v_rel=(dot(v_rel,dir_v)./norm(dir_vel)).*(dir_v./norm(dir_v));
+
+dop=v_rel/c;
+
+%the change in frequency = dop * initial_frequency;
+%OR
+% new frequency=initial frequency*(1+dop)
