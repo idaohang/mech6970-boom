440880ed4fe4346aad690f6bb12498c6bb5e4d84
Robert Cofield <robertgcofield@gmail.com>
2013-12-12 13:26:43 -0600

    have doppler calculated from imu

--- proj/calcDoppler.m
+++ proj/calcDoppler.m
@@ -1,16 +1,30 @@
-function dop=calcDoppler(user_v, sat_v, dir_v)
-%user velocity vector, satelltie velocity vector, direction vector (user to
+function dop=calcDoppler(user_pos, course, user_vel, sat_v, sat_pos)
+%user position(lat,long,alt), LLA, course, user velocity magnitude (along course), satelltie position vector, direction vector (user to
 %satellite).  All in same reference frame.
+lat=user_pos(1);
+long=user_pos(2);
 
 c=299792458;
 
+vel_E=cos(course)*user_vel;
+vel_N=sin(course)*user_vel;
+vel_U=0;
+vel_ENU=[vel_E,vel_N,vel_U];
+
+user_pos_ecef = wgslla2xyz(user_pos(1), user_pos(2), user_pos(3));
+dir_v=sat_pos-user_pos_ecef';
+
+user_v=rotenu2xyz(vel_ENU', lat, long);
+
 dir_v=dir_v./norm(dir_v);
-v_rel=user_v-sat_v;
+v_rel=user_v-sat_v';
 
-v_rel=(dot(v_rel,dir_v)./norm(dir_vel)).*(dir_v./norm(dir_v));
+v_rel=(dot(v_rel,dir_v)./norm(dir_v)).*(dir_v./norm(dir_v));
 
-dop=v_rel/c;
+dop=norm(v_rel)/c;
+freqL1 = 154*10.23e6;
 
+dop=dop*freqL1;
 %the change in frequency = dop * initial_frequency;
 %OR
 % new frequency=initial frequency*(1+dop)
--- proj/main2.m
+++ proj/main2.m
@@ -1,19 +1,54 @@
 %% Numerically Integrate Crossbow
 
-clear
+clear all; close all; clc
 load workspace_after_time_sync
 
-% XBow_accY_filt = zeros(size(XBow_accY));
-%h = fdesign.lowpass('Fp,Fst,Ap,Ast', 0.15, );
-%d = design(h,'equiripple');
-[b,a]=butter(4, .01, 'low');
+[b,a]=butter(4, .005, 'low');
+
 
 for k = 1:4
   XBow_accY_filt(:,k) = filtfilt(b,a,XBow_accY(:,k));
 end
 
+% remove gravity
+XBow_accY_filt = XBow_accY_filt - mean(XBow_accY_filt(1:1000,1));
+
 plot(XBow_accY(:,1)); hold on
 plot(XBow_accY_filt(:,1), 'g')
 
+dTOW = mean(mean(diff(TOW(:,1:4))));
+for ch = 1:4
+  XBow_velY(:,ch) = cumtrapz(XBow_accY_filt(:,ch)) * dTOW;
+end
+XBow_velY = XBow_velY(1:end-1,:);
+XBow_velY = [zeros(1,4); XBow_velY];
 
-%% Our Tracking comparison
+figure;
+plot(XBow_velY(:,1))
+
+
+%% calc doppler
+
+userpos_lla = [gNovatel.zLat(1), gNovatel.zLong(1), gNovatel.zHeight(1)];
+
+% interpolate GPS course
+raw_course = unwrap(gNovatel.zCourse/360*2*pi);
+imu_dopp = zeros(length(TOW),4);
+% course = zeros(length(TOW),4);
+
+parfor k = 1:4
+  course = interp1(gNovatel.zGPSSeconds/1000, raw_course, TOW(:,k));
+  course = rem(course(:,k),2*pi); 
+  for kk = 1:length(TOW)
+    imu_dopp(kk,k) = calcDoppler(userpos_lla,course(kk), XBow_velY(kk,k), svvel_nordnav{kk,k}, svpos_nordnav{kk,k});
+  end
+end
+
+figure;
+plot(TOW(:,1:4),imu_dopp)
+grid on
+title('Doppler resultant from integrated IMU');
+legend('32','16','20','31')
+ylabel('f_{Dopp} (Hz)')
+xlabel('GPS Time (sec)');
+
