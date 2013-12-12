ecd96f58c92412eecba32aafdfed14aa0478542f
Robert Cofield <robertgcofield@gmail.com>
2013-12-12 13:42:21 -0600

    doppler plot

--- proj/main2.m
+++ proj/main2.m
@@ -31,24 +31,27 @@ plot(XBow_velY(:,1))
 
 userpos_lla = [gNovatel.zLat(1), gNovatel.zLong(1), gNovatel.zHeight(1)];
 
+stop_idx =  50886; % data is bad after this
+
 % interpolate GPS course
 raw_course = unwrap(gNovatel.zCourse/360*2*pi);
 imu_dopp = zeros(length(TOW),4);
 % course = zeros(length(TOW),4);
 
-parfor k = 1:4
+for k = 1
   course = interp1(gNovatel.zGPSSeconds/1000, raw_course, TOW(:,k));
   course = rem(course(:,k),2*pi); 
   for kk = 1:length(TOW)
     imu_dopp(kk,k) = calcDoppler(userpos_lla,course(kk), XBow_velY(kk,k), svvel_nordnav{kk,k}, svpos_nordnav{kk,k});
   end
+  
 end
 
-figure;
-plot(TOW(:,1:4),imu_dopp)
+f = figure;
+plot(TOW(:,1),imu_dopp(:,1))
 grid on
 title('Doppler resultant from integrated IMU');
 legend('32','16','20','31')
 ylabel('f_{Dopp} (Hz)')
 xlabel('GPS Time (sec)');
-
+saveas(f, 'Dopplers_over_time.png')
